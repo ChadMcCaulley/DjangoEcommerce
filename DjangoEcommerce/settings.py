b01b63dfca8fcac7fc18ceb02f6e5b8181fc44a2
Chad McCaulley <chad.mccaulley@pgm-corp.com>
2020-07-14 12:59:19 -0700

    Added core app

--- DjangoEcommerce/settings.py
+++ DjangoEcommerce/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'allauth',
     'allauth.account',
-    'allauth.socialaccount'
+    'allauth.socialaccount',
+    'core'
 ]
 
 MIDDLEWARE = [
