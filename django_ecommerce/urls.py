ed278de84b391a39336292d7bac9b94247fb077c
Chad McCaulley <chad.mccaulley@pgm-corp.com>
2020-07-22 21:44:14 -0700

    Changed from /api to /core for better naming conventions

--- django_ecommerce/urls.py
+++ django_ecommerce/urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('dj_rest_auth.urls')),
     path('auth/registration/', include('dj_rest_auth.registration.urls')),
-    path('api/', include('core.urls'))
+    path('core/', include('core.urls'))
 ]
