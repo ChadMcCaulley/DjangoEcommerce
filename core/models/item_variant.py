fa91e7e2ca8a7a4734fd8a11a6332761b8c5a278
Chad McCaulley <chad.mccaulley@pgm-corp.com>
2020-07-22 21:54:51 -0700

    Added verbose name plural for more consistent admin panel naming

--- core/models/item_variant.py
+++ core/models/item_variant.py
@@ -3,6 +3,9 @@ from core.datatypes import NullableDecimal
 from core.mixins import TimeStampMixin
 
 class ItemVariant(TimeStampMixin):
+    class Meta:
+        db_table='item_variant'
+        verbose_name_plural='Item Variants'
     title = models.CharField(max_length=100)    
     price = models.DecimalField(max_digits=15, decimal_places=2)
     list_price = NullableDecimal(max_digits=15, decimal_places=2)
