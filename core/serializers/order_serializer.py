c76208ebc61cf3b973c2097204f58019bc52e086
Chad McCaulley <chad.mccaulley@pgm-corp.com>
2020-07-23 23:07:21 -0700

    Added items to order serializer

--- core/serializers/order_serializer.py
+++ core/serializers/order_serializer.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
 from core.models import Order
+from core.serializers.item_serializer import ItemSerializer
 
 class OrderSerializer(serializers.HyperlinkedModelSerializer):
+    items = ItemSerializer(many=True)
+
     class Meta:
         model = Order
         fields = [
