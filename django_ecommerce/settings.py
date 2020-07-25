9a8a3d4a14f87349cb00edbc10e92fa4b9d0e451
Chad McCaulley <chad.mccaulley@pgm-corp.com>
2020-07-25 12:58:44 -0700

    Added page number pagination app wide

--- django_ecommerce/settings.py
+++ django_ecommerce/settings.py
@@ -139,7 +139,9 @@ USE_TZ = True
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100
 }
 
 SIMPLE_JWT = {
