2559eae0d2adb6c5bebde0b9ccd88d0f9c65676d
Chad McCaulley <chad.mccaulley@pgm-corp.com>
2020-07-25 22:50:38 -0700

    Changed serializers based on model changes

--- core/serializers/item_serializer.py
+++ core/serializers/item_serializer.py
@@ -2,21 +2,15 @@ from rest_framework import serializers
 from core.models import Item
 from core.serializers.item_variant_serializer import \
     ItemVariantSerializer
-from core.serializers.image_serializer import ImageSerializer
 
 class ItemSerializer(serializers.ModelSerializer):
     variants = ItemVariantSerializer(many=True, read_only=True)
-    images = ImageSerializer(many=True, read_only=True)
 
     class Meta:
         model = Item
         fields = [
             'id',
             'title',
-            'max_price',
-            'min_price',
-            'rating',
-            'num_ratings',
             'created_at',
             'updated_at',
             'variants'
--- core/serializers/item_variant_serializer.py
+++ core/serializers/item_variant_serializer.py
@@ -1,7 +1,11 @@
 from rest_framework import serializers
 from core.models import ItemVariant
+from core.serializers.image_serializer import ImageSerializer
+
 
 class ItemVariantSerializer(serializers.ModelSerializer):
+    images = ImageSerializer(many=True, read_only=True)
+
     class Meta:
         model = ItemVariant
         fields = [
@@ -14,4 +18,5 @@ class ItemVariantSerializer(serializers.ModelSerializer):
             'created_at',
             'updated_at',
             'num_ratings',
+            'images'
         ]
