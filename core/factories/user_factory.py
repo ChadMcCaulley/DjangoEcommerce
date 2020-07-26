21ff71c61252a65677764c559bdba6c7dac11b05
Chad McCaulley <chad.mccaulley@pgm-corp.com>
2020-07-26 15:50:36 -0700

    Made users more random (avoids collisions for get)

--- core/factories/user_factory.py
+++ core/factories/user_factory.py
@@ -5,10 +5,12 @@ from django.conf import settings
 class UserFactory (factory.DjangoModelFactory):
     class Meta:
         model = settings.AUTH_USER_MODEL
-        django_get_or_create = ('email','username')
+        django_get_or_create = ('email','username', 'first_name', 'last_name')
 
-    username = factory.Faker('name')
-    email = factory.Faker('ascii_free_email')
+    username = factory.Faker('text', max_nb_chars=50)
+    first_name = factory.Faker('name')
+    last_name = factory.Faker('name')
+    email = factory.Faker('ascii_company_email')
     password = factory.PostGenerationMethodCall(
         'set_password', 'password')
     
