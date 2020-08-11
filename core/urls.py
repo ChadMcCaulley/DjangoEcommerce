4108a7f2a11a313fa609fd00b7156ddf2c4be112
Chad McCaulley <chad.mccaulley@pgm-corp.com>
2020-08-10 21:33:53 -0700

    Urls changed to use new views and model names

--- core/urls.py
+++ core/urls.py
@@ -1,18 +1,18 @@
 from django.urls import path
 from rest_framework import routers
 from .views import (
-    ItemImageView,
-    ItemView,
-    ItemVariantView,
+    ProductImageView,
+    ProductLineView,
+    ProductView,
     OrderView
 )
 
 app_name = 'core'
 
 router = routers.DefaultRouter()
-router.register(r'item_images', ItemImageView)
-router.register(r'items', ItemView)
-router.register(r'item_variants', ItemVariantView)
+router.register(r'product_images', ProductImageView)
+router.register(r'product_lines',   ProductLineView)
+router.register(r'products', ProductView)
 router.register(r'orders', OrderView)
 
 urlpatterns = router.urls
