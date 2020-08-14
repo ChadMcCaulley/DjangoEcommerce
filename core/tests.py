2096d2ea4e4f842599e9a50f1777191786726816
Chad McCaulley <chad.mccaulley@pgm-corp.com>
2020-08-14 16:32:04 -0700

    Added test for product review signals

--- core/tests.py
+++ core/tests.py
@@ -3,16 +3,17 @@ from core.models import Product
 from core.factories import ProductFactory, ReviewFactory
 
 class ProductTests (TestCase):
-    def setUp(self):
-        self.product = ProductFactory()
 
-    def test_product_review_signal(self):
-        """Product rating changed when review added"""
-        self.assertEqual(self.product.rating, None)
-        self.assertEqual(self.product.num_reviews, 0)
-        ReviewFactory(rating=2, product=self.product)
-        ReviewFactory(rating=5, product=self.product)
-        ReviewFactory(rating=2, product=self.product)
-        ReviewFactory(rating=5, product=self.product)
-        self.assertEquals(self.product.rating, 3.5)
-        self.assertEquals(self.product.num_reviews, 4)
+    def test_product_review_signals (self):
+        """Product rating changes when reviews are added and removed"""
+        product = ProductFactory()
+        self.assertEqual(product.rating, None)
+        self.assertEqual(product.num_reviews, 0)
+        review = ReviewFactory(rating=1, product=product)
+        review2 = ReviewFactory(rating=5, product=product)
+        self.assertEquals(product.rating, 3)
+        self.assertEquals(product.num_reviews, 2)
+        review.delete()
+        review2.delete()
+        self.assertEquals(product.rating, None)
+        self.assertEquals(product.num_reviews, 0)
