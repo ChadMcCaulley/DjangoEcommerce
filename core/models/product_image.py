36f8603a635f49a71620b0ee6f41f20f6b813237
Chad McCaulley <chad.mccaulley@pgm-corp.com>
2020-08-14 20:46:18 -0700

    Updated product image upload path. Added rating breakdown action to product view

--- core/models/product_image.py
+++ core/models/product_image.py
@@ -3,13 +3,15 @@ from core.mixins import TimeStampMixin
 
 def get_upload_path(instance, filename):
     product = instance.product
-    return f'images/{product.title}_{product.id}/{filename}'
+    return f'images/{product.company.name}/{product.id}/{filename}'
 
 
 class ProductImage(TimeStampMixin):
     class Meta:
         verbose_name_plural='Product Images'
-    image = models.ImageField(upload_to=get_upload_path)
+    image = models.ImageField(
+        upload_to=get_upload_path, max_length=300
+    )
     product = models.ForeignKey(
         'Product', on_delete=models.CASCADE, related_name='images'
     )
--- core/views/product.py
+++ core/views/product.py
@@ -1,9 +1,13 @@
 from django_filters import FilterSet
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from core.models import Product
-from core.serializers import ProductSerializer
+from core.serializers import (
+    ProductSerializer, RatingBreakdownSerializer
+)
 
 class ProductFilterSet(FilterSet):
     class Meta:
@@ -16,3 +20,9 @@ class ProductView(viewsets.ModelViewSet):
     serializer_class = ProductSerializer
     filter_backends = [DjangoFilterBackend]
     filter_class = ProductFilterSet
+
+    @action(detail=True, methods=['get'])
+    def rating_breakdown (self, request, pk=None):
+        variant = self.get_object()
+        serializer = RatingBreakdownSerializer(variant)
+        return Response(serializer.data)
